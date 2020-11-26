050399103f1aaccf25ad0deaa0ab280c60b5f94f
Vincent Wilantara <comoestassenor@gmail.com>
2020-11-26 10:40:51 +0100

    updated gameoflife files

---
+++ src/test/java/GameOfLifeTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GameOfLifeTest {
+
+    @Test
+    public void test0Neighbors(){
+        GameOfLife gm = new GameOfLife();
+        assertThat(gm.numberOfNeighbors()).isEqualTo(0);
+    }
+}
