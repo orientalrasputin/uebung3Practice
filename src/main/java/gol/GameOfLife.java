fe2a34ae668c1b166933b86f76a64231361f5399
Vincent Wilantara <comoestassenor@gmail.com>
2020-11-26 12:14:46 +0100

    added functions

--- src/main/java/gol/GameOfLife.java
+++ src/main/java/gol/GameOfLife.java
@@ -1,12 +1,13 @@
 package gol;
 
 
-public class GameOfLife {// jsadnfisadfn
-
+public class GameOfLife {
+    private int[][] field;
     private final int param;
 
-    public GameOfLife(final int param){
+    public GameOfLife(int[][] field, final int param){
         this.param = param;
+        this.field = field;
     }
 
     public int getParam(){
@@ -20,4 +21,17 @@ public class GameOfLife {// jsadnfisadfn
     public int numberOfNeighbors2(){
         return 0;
     }
+
+    /**
+     functions:
+
+     func(int[][])
+         isAlive() - Main Class - check if the cell has a living Creature in it
+         spawnCreature() - Main Class - if empty field has 3 neighbors, then create a creature on next round
+         countNeighbors(int[][]) - Main Class
+         willSurvive() - Main Class - check if neighbor == 2 or 3, if yes then return true
+         iterateField() - Main Class - Iterator
+
+     initialize() - Test - initialize the mock Field
+     **/
 }
